748765f93a98e43af64353674ae1f042d29a0d07
donpedromz <juanpabloolavemunoz2006@gmail.com>
2026-05-25 10:13:41 -0500

    feat(service-audit): added audit integration with services module

--- src/modules/services/services.module.ts
+++ src/modules/services/services.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { ServicesController } from './controllers/services.controller';
 import { ServiceRepository } from './repositories/service.repository';
 import { SERVICE_REPOSITORY } from './repositories/service.repository.interface';
+import { ServicesAuditService } from './services/services-audit.service';
 import { ServicesService } from './services/services.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [ServicesController],
   providers: [
     ServicesService,
+    ServicesAuditService,
     {
       provide: SERVICE_REPOSITORY,
       useClass: ServiceRepository,
